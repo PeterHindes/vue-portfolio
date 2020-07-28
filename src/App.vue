3e9d67ee1840f34047d23f64dbf494e0a8411494
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2020-07-28 17:58:09 -0600

    hmmmmmmm
    duplicate fetches rip

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img loading="lazy" alt="Vue logo" src="./assets/logo.png" />
+    <!--<div v-for="item in fdata" :key="item">
+      <div v-for="itemm in item" :key="itemm">
+        <Element :textt="itemm" />
+      </div>
+    </div>-->
+    <component v-bind:is="comp" msg="Hello"></component>
+    <button v-on:click="comp = 'HelloWorld2'">Hi</button>
+    <button v-on:click="comp = 'HelloWorld'">Bye</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Element,
+    HelloWorld: () => import( /* webpackPreload: true */ './components/HelloWorld.vue'),
+    HelloWorld2: () => import( /* webpackPreload: true */ './components/HelloWorld2.vue'),
+  },
+  data: function () {
+    return {
+      //fdata: jd().jsonData.then(function(result) {return}),
+      comp: "HelloWorld"
+    }
   }
 }
 </script>
@@ -22,7 +35,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: beige;
   margin-top: 60px;
 }
+body {
+  background-color: black;
+}
+
 </style>
---
+++ src/components/Element.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="element">
+      <h1>{{textt}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Element',
+    props: {
+        textt: String
+    }
+}
+</script>
+
+<style>
+
+</style>
